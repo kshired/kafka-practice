a6821b05c70acfaa2e3a336d482c2f0327cf98e2
kshired <shiroed1211@gmail.com>
2024-01-29 03:08:27 +0900

    config : add jackson

--- practice/build.gradle.kts
+++ practice/build.gradle.kts
@@ -6,4 +6,7 @@ dependencies {
     implementation(project(":common"))
     implementation("com.github.javafaker:javafaker:${property("javaFakerVersion")}")
     implementation("org.postgresql:postgresql:${property("postgresqlVersion")}")
+    implementation("com.fasterxml.jackson.core:jackson-databind:${property("jacksonVersion")}")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:${property("jacksonVersion")}")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:${property("jacksonVersion")}")
 }
