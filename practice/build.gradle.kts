9775fe479a10b8aa5b27c89326b76171586571b9
kshired <shiroed1211@gmail.com>
2024-01-29 02:38:04 +0900

    config : add postgresql

--- practice/build.gradle.kts
+++ practice/build.gradle.kts
@@ -5,4 +5,5 @@ tasks.getByName("jar") {
 dependencies {
     implementation(project(":common"))
     implementation("com.github.javafaker:javafaker:${property("javaFakerVersion")}")
+    implementation("org.postgresql:postgresql:${property("postgresqlVersion")}")
 }
