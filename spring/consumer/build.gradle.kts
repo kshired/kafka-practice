0e5c97389ffe1d553f6c9b7e21bf2e995488148f
kshired <shiroed1211@gmail.com>
2024-02-04 05:03:04 +0900

    remove : useless dependencies

--- spring/consumer/build.gradle.kts
+++ spring/consumer/build.gradle.kts
@@ -13,6 +13,5 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.postgresql:postgresql")
     implementation(project(":spring:kafka"))
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:${property("jacksonVersion")}")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:${property("jacksonVersion")}")
 }
